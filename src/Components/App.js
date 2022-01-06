d8d18bdafcde2ffec0e6a1e00bc66aada7120675
nayruthCalla <nayruth.calla@gmail.com>
2022-01-05 19:42:27 -0500

    first mobile

--- src/Components/App.js
+++ src/Components/App.js
@@ -15,16 +15,31 @@ const Container = styled.div`
 `;
 const Image = styled.img``;
 const Figure = styled.figure`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 1rem;
+  display: none;
+  @media screen and (min-width: 768px) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+    flex: 1;
+  }
+  @media screen and (min-width: 1024px) {
+  }
 `;
 const Title = styled.h1`
   font-family: Montserrat;
   font-style: normal;
   font-weight: bold;
   font-size: 30px;
+  line-height: 37px;
+  margin: 1rem 0 0 0;
+  color: rgba(255, 255, 255, 0.54);
+`;
+const TitleFooter = styled.p`
+  font-size: 10px;
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: bold;
   line-height: 37px;
   color: rgba(255, 255, 255, 0.54);
 `;
@@ -37,6 +52,7 @@ function App() {
         <Image src={ma5} alt="" />
       </Figure>
       <TimerTemplate />
+      <TitleFooter>Made by Nayruth Calla @ 2021</TitleFooter>
     </Container>
   );
 }
